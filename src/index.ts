4f4bc00c1c0f3c269eed141a4a32e507af3b93b5
Prince Yadav <princeyadav392005@gmail.com>
2026-05-10 14:36:26 +0530

    rebranding the app and shit

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,20 @@ import { chooseFramework, choosePackageManager } from '@/prompts/basic';
 import { execFunc, mapper } from './utils/frameworkMapper';
 const program = new Command();
 
+program
+  .name('Rebar')
+  .description('Interactive CLI for scaffolding JavaScript and TypeScript projects across multiple frameworks.')
+  .version('1.0.0');
+
 program.command("init")
   .action(async () => {
     const manager = await choosePackageManager();
     const framework = await chooseFramework(manager);
     const func:execFunc = mapper[framework]
     if(!func) process.exit(1) 
-    func(manager)
+    await func(manager)
+    console.log("firse babay")
   });
-console.log('your nextjs app has been installed')
+
 
 program.parse();
